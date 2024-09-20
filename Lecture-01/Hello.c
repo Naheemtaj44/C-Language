da6b7bd7c898ef8ff695e1176872fcc21b4d174c
naheemtaj44 <naheemtaj44@gmail.com>
2024-09-20 12:43:40 +0500

    practice question

--- Lecture-01/Hello.c
+++ Lecture-01/Hello.c
@@ -1,5 +1,33 @@
 # include<stdio.h>
 int main(){
-printf ("hello world");
+// printf ("hello world\n");
+// printf ("hello world\n");
+// printf ("hello world\n");
+// printf ("hello world\t");
+// int i = 0;
+// printf ("i is %d=",i);
+// int age = 12;
+// printf ("age is %d\n",age);
+
+
+
+// age input and print again age
+// int age;
+//   printf("enter ur age");
+// scanf("%d",&age);
+// printf("age is %d",age);
+
+
+
+// sum of two numbers
+// int a;
+// printf("rnter a:");
+// scanf("%d",&a);
+// int b;
+// printf("enter b:");
+// scanf("%d",&b);
+// int sum = a+b;
+// printf("sum is :%d",sum);
+
 return 0;
 }
---
+++ Lecture-01/PQ.c
@@ -0,0 +1,21 @@
+/*PRACTICE QUESTION NO:01
+ find Area of Square*/
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int side;
+    printf("Entr side ");
+    scanf("%d",&side);
+    printf("the Area is equal to : %d",side*side);
+
+/*PRACTICE QUESTION NO:02
+ find Area of cicle
+ farmula=pi*reduis*reduis  */
+float reduis;
+printf("enter reduis");
+scanf("%f",&reduis);
+float pi=3.14;
+printf ("area is %f",pi*reduis*reduis);
+
+    return 0;
+}
