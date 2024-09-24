2eb3e0f96dc5d61be045344b423aac9125b7971b
naheemtaj44 <naheemtaj44@gmail.com>
2024-09-24 15:12:01 +0500

    add practice question

---
+++ Lecture-01/PQ1.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+#include<math.h>
+int main(){
+    /*PRACTICE QUESTION NO:02
+ find Area of cicle
+ farmula=pi*reduis*reduis  */
+float reduis;
+printf("enter reduis");
+scanf("%f",&reduis);
+float pi=3.14;
+printf ("area is %f",pi*reduis*reduis);
+
+    return 0;
+}
